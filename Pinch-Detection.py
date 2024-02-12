60ec89e012b06193a5f5070dfad1bb0fdad5a6a9
casual <malak.john111@gmail.com>
2024-02-12 11:00:32 -0800

    ehm yea its not working!

--- Pinch-Detection.py
+++ Pinch-Detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 
 import mediapipe as mp
+import pyautogui 
 
 
 #I guess some of the names in mediapipe are really long so we r shortening them here
@@ -16,6 +17,18 @@ mp_hands = mp.solutions.hands
 vid = cv2.VideoCapture(0)
 vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+
+x_Index_finger = 0
+y_Index_finger = 0
+x_thumb = 0
+y_thumb = 0
+rouneded_x_Index_finger = 0
+rouneded_x_thumb = 0
+rouneded_y_Index_finger = 0
+rouneded_y_thumb = 0
+upper_bound_x = 0
+lower_bound_x = 1000
+
 
 with mp_hands.Hands(
     model_complexity=1,
@@ -41,6 +54,29 @@ with mp_hands.Hands(
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
         cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+
+
+
+                x_Index_finger = hand_landmarks.landmark[8].x  
+                y_Index_finger = hand_landmarks.landmark[8].y  
+                x_thumb = hand_landmarks.landmark[4].x
+                y_thumb = hand_landmarks.landmark[4].y
+
+
+            rouneded_x_Index_finger = round(x_Index_finger, 4)
+            rouneded_x_thumb = round(x_thumb, 4)
+            print(rouneded_x_Index_finger, rouneded_x_thumb)
+
+
+        upper_bound_x = x_Index_finger + 0.01
+        lower_bound_x = x_Index_finger - 0.01
+        #print(upper_bound_x, lower_bound_x)
+        if upper_bound_x > rouneded_x_Index_finger > lower_bound_x:
+            print("detected")
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 vid.release()
@@ -58,19 +94,3 @@ vid.release()
 
 
 
-while True:
-    ret, frame = vid.read()
-#declares the variable Ret and Frame and gives both of those Vars the value of vid.read(), which means that they read the incoming video 
-
-    cv2.imshow('frame', frame)
-#then it shows the Frame which we declared in the last line, and gives it the name of frame (and since we declared frame as the camera in the last line, it just displays the camera)
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-#if q key is pressed it stops the while loop which forces the program to go to the next couple lines 
-
-#closes the lines
-vid.release()
-
-cv2.destroyAllWindows()
